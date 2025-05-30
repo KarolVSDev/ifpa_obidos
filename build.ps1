3edc327d9b046fd6c0546d70bce27bd2599a8d85
Ana <ana.souza@tce.am.gov.br>
2025-05-30 07:40:23 -0400

    excluindo arquivos desnecessários

--- build.ps1
+++
@@ -1,3 +0,0 @@
-$exclude = @("venv", "bot_covid.zip")
-$files = Get-ChildItem -Path . -Exclude $exclude
-Compress-Archive -Path $files -DestinationPath "bot_covid.zip" -Force
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -r "bot_covid.zip" * -x "bot_covid.zip"
